aa397f0457a2d828f99349de8aa0448fea479f4e
Not Zero <theyspoof@gmail.com>
2025-07-13 14:00:33 -0300

    Feat: Vue Vapor
    New metronome sounds

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { createApp } from "vue";
+import { createVaporApp, type VaporComponent } from "vue";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+createVaporApp(App as VaporComponent).mount("#app");
